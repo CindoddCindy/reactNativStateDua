63bacc8f4582c43cc633e98ef6325c515224fadf
CindoddCindy <cindodcindy@gmail.com>
2020-10-07 14:27:55 +0700

    react state dua

---
+++ reactDuaState/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+   state = {
+      myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmo tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quisnostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.'
+   }
+   render() {
+      return (
+      <View>
+         <Text> {this.state.myState} </Text>
+      </View>
+      );
+   }
+}
